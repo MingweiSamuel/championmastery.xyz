94bb53b1b80d50d195b550bd22b7c47a6a936244
Mingwei Samuel <mingwei.samuel@gmail.com>
2016-04-22 19:59:27 +0000

    responsiveness fixes

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ var champApp = angular.module('champApp', []);
 var baseUrl = 'https://script.google.com/macros/s/AKfycbyOAvaQmTEtU8HY70Pw5t_V45bFxszORZ_wwcbIu_jmVF-msAU/exec?json=true';
 
 champApp.controller('MainController', function($scope, $http) {
-  var url = baseUrl + '&' + document.location.search.substr(1);
+  var params = document.location.search.substr(1);
+  var url = baseUrl + '&' + params;
+  
+  params = params.split('&');
+  for (var i = 0; i < params.length; i++) {
+    var pair = params[i].split('=');
+    if (pair.length < 2)
+      continue;
+    if (pair[0] === 'summoner')
+      $scope.summoner = pair[1];
+    if (pair[0] === 'region')
+      $scope.region = pair[1];
+  }
   $scope.regions = ["NA", "BR", "EUNE", "EUW", "KR", "LAN", "LAS", "OCE", "TR", "RU"];
+  
   $http.get(url).then(function(res) {
     console.log('response', res);
     $scope.loaded = true;
